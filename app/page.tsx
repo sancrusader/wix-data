9161615aa82bf352a21a82314abb06cd1558c66a
Sandi Fox <sandimixyz@gmail.com>
2024-11-13 02:08:13 +0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Component() {
   const [cordinate, setCordinate] = useState('');
